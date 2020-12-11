55d6be27c7271a104bd3f9af34dd87b91fb64852
sjagoori <sjagoori@users.noreply.github.com>
2020-12-11 19:26:51 +0100

    Added API call - resolve CORS issue

---
+++ components/utils.js
@@ -0,0 +1,6 @@
+import axios from 'axios'
+
+export const getData = async (cat, loc) => {
+  let base = 'https://api.bol.com/' + cat + '/v4/' + loc + '?apikey=' + process.env.API_KEY + '&offers=cheapest&includeAttributes=false&format=json'
+  return await axios.get(base).then(response => response.data)
+}
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import { getData } from '../components/utils'
 
 export default class Index extends React.Component {
   constructor(props) {
     super(props)
+  }
+
+  async componentDidMount() {
+    console.log(await getData('catalog','products/1004004011187773'))
   }
 
   render() {
