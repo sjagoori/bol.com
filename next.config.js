219f218d9e873b09951a532187518cf142eb831a
sjagoori <s.jagoori@outlook.com>
2020-12-11 15:46:38 +0100

    Init

---
+++ next.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  env: {
+    API_KEY: process.env.API_KEY,
+    API_SECRET: process.env.API_SECRET,
+  }
+}
---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default class Index extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <>
+        Hello World!
+      </>
+    )
+  }
+}
