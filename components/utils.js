78a807a7b734e03a08baa29722462786d11aaedd
sjagoori <sjagoori@users.noreply.github.com>
2020-12-13 16:36:07 +0100

    Added products from bol-helper + localstorage, and generating products

--- components/utils.js
+++ components/utils.js
@@ -1,6 +1,16 @@
 import axios from 'axios'
 
-export const getData = async (cat, loc) => {
-  let base = 'https://shacors.herokuapp.com/https://api.bol.com/' + cat + '/v4/' + loc + '?apikey=' + process.env.API_KEY + '&offers=cheapest&includeAttributes=false&format=json'
-  return await axios.get(base).then(response => response.data)
+export const getData = async (productid) => {
+  let proxy = 'https://shacors.herokuapp.com/'
+  let link = 'https://api.bol.com/catalog/v4/products/' + productid + '?apikey=' + process.env.API_KEY + '&format=json'
+  let base = proxy + link
+  return await axios.get(base).then(response => response.status == 200 ? response.data.products[0] : false).catch(error => console.log(error.response))
+}
+
+export const getProducts = async (url) => {
+  return await axios.get(url).then(response => response.data)
+}
+
+export const filterObject = dataset => {
+  return dataset.filter(item => typeof item === 'object')
 }
--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,46 @@
 import React from 'react'
-import { getData } from '../components/utils'
+import Layout from '../components/Layout'
+import Product from '../components/Product'
+import { getData, getProducts, filterObject } from '../components/utils'
 
 export default class Index extends React.Component {
   constructor(props) {
     super(props)
+    this.state = { state: false }
   }
 
   async componentDidMount() {
-    console.log(await getData('catalog','products/1004004011187773'))
+    console.warn('componentDidMount:', this.state.state)
+    this.setState({ products: await getProducts('https://bol-helper.herokuapp.com/v1') })
+    localStorage.getItem('postProduct') == null ? this.setProducts() : (JSON.parse(localStorage.getItem('postProduct')), this.setState({ state: true }))
+  }
+
+  async setProducts() {
+    console.warn('setProducts:', this.state.state)
+    let preProduct = this.state.products.map(async key => Promise.resolve(await getData(key)))
+    let postProduct = filterObject(await Promise.all(preProduct))
+    localStorage.setItem('postProduct', JSON.stringify(postProduct))
+    this.setState({ products: postProduct, state: true })
+  }
+
+  generateProducts() {
+    console.warn('generateProducts:', this.state.state)
+    return JSON.parse(localStorage.getItem('postProduct')).map((value, index) => {
+      return <Product
+        key={index}
+        title={value.title}
+        img={value.images[5].url}
+        price={value.offerData.offers[0].price}
+      />
+    })
   }
 
   render() {
+    console.warn('render:', this.state.state)
     return (
-      <>
-        Hello World!
-      </>
+    <Layout>
+        {this.state.state ? this.generateProducts() : <div>Loading</div>}
+      </Layout>
     )
   }
 }
