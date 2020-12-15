4b7655478b2979a0e5409ff825ffa1f07c045fe1
Nathan <nathanbommezijn@gmail.com>
2020-12-16 00:28:39 +0100

    fix whitespace after import #1

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 import React from 'react'
 import Link from 'next/link'
+
 export default class Navbar extends React.Component {
   constructor(props) {
     super(props);
