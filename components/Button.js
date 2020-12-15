b15e6eada5b1b09d962e05855c6d5aa7b9704598
sjagoori <sjagoori@users.noreply.github.com>
2020-12-15 23:27:36 +0100

    formatting

--- components/Button.js
+++ components/Button.js
@@ -14,7 +14,7 @@ export default class Button extends React.Component {
       <ThemeProvider theme={theme}>
         <Link href={this.props.url}>
           <Container type={this.props.type}>
-              <span><Icon /></span>
+              <Icon />
               <span>{this.props.label}</span>
           </Container>
         </Link>
@@ -27,14 +27,6 @@ const Container = styled.div`
   text-decoration: none;
   text-transform: capitalize;
   color: ${props => props.theme.colors.white};
-  
-  span {
-    margin-left: -10px;
-
-    :first-child {
-      margin-right: 5px;
-    }
-  }
 
   cursor: pointer;
   display: block;
@@ -50,16 +42,17 @@ const Container = styled.div`
     if (props.type == 'secondary') return props.theme.colors.secondary;
   }};
 
+svg {
+    margin-left: -15px;
+    fill: white;
+    opacity: 0;
+    margin-right: 10px;
+    transition:  opacity .1s ease-in-out;
+  }
+
   :hover{
      svg {
       opacity: 1;
     }
-  }
-
-  svg{
-    fill: white;
-    opacity: 0;
-    margin-right: 15px;
-    transition:  opacity .1s ease-in-out;
   }
 `
