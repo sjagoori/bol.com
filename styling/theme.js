c315af9754243b7a93f55c3f91830e79f5ddeedf
sjagoori <sjagoori@users.noreply.github.com>
2020-12-16 22:31:50 +0100

    Added colors

--- styling/theme.js
+++ styling/theme.js
@@ -3,7 +3,9 @@ export default {
     primary: "#36308F",
     secondary: "#36308F",
     black: "#1D1D1D",
-    white: "white"
+    white: "white",
+    red: "red",
+    green: "#27cb73"
   },
   backgroundColor:'#FBF1F4'
 }
